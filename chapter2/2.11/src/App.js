a11f469108f557a06100d5cf727f5a17e257254b
dhanu <andreasdhanu@gmail.com>
2020-04-14 20:43:10 +0700

    fin

--- chapter2/2.11/src/App.js
+++ chapter2/2.11/src/App.js
@@ -8,8 +8,8 @@ const App = () => {
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
-    .then(response => {
-      setCountries(response.data)
+      .then(response => {
+        setCountries(response.data)
     })
   }, [])
 
@@ -17,12 +17,16 @@ const App = () => {
     setFilter(event.target.value)
   }
 
+  const handleShowClick = (event) => {
+    setFilter(event.target.value)
+  }
+
   const countriesToShow = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
 
   return (
     <div>
-      <Filter handle={handleFilterChange} />
-      <Countries countries={countriesToShow} isFiltered={filter.length>0}/>
+      <Filter handle={handleFilterChange} value={filter}/>
+      <Countries countries={countriesToShow} isFiltered={filter.length>0} handleShowClick={handleShowClick} />
     </div>
   )
 }
@@ -30,7 +34,7 @@ const App = () => {
 const Filter = (props) => {
   return (
     <div>
-      find countries <input onChange={props.handle} />
+      find countries <input onChange={props.handle} value={props.value} />
     </div>
   )
 }
@@ -52,7 +56,7 @@ const Countries = (props) => {
     return (
       <div>
         {props.countries.map(country => 
-          <Country key={country.alpha2Code} country={country} showFull={true}/>
+          <Country key={country.alpha2Code} country={country} showFull={true} handleShowClick={props.handleShowClick} />
         )}
       </div>
     )
@@ -60,7 +64,7 @@ const Countries = (props) => {
     return (
       <div>
         {props.countries.map(country => 
-          <Country key={country.alpha2Code} country={country} />
+          <Country key={country.alpha2Code} country={country} handleShowClick={props.handleShowClick} />
         )}
       </div>
     )
@@ -74,20 +78,62 @@ const Country = (props) => {
         <h1>{props.country.name}</h1>
         <div>capital {props.country.capital}</div>
         <div>population {props.country.population}</div>
-        <h2>languages</h2>
+        <h2>Spoken languages</h2>
         <ul>
           {props.country.languages.map(language =>
-            <li>{language.name}</li>
+            <li key={language.name}>{language.name}</li>
           )}
         </ul>
-        <img src={props.country.flag} height="100" width="100"/>
+        <img src={props.country.flag} alt={`${props.country.name} flag`} height="100" width="100"/>
+        <Weather country={props.country} />
       </div>
     )
   } else {
     return (
-      <div>{props.country.name}</div>
+      <div>{props.country.name}
+        <Button text="show" countryName={props.country.name} handleClick={props.handleShowClick} />
+      </div>
     )
   }
+}
+
+const Weather = (props) => {
+  const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${props.country.capital}`)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [props.country.capital])
+
+  if (weather.current !== undefined) {
+    return (
+      <div>
+        <h2>{`Weather in ${props.country.capital}`}</h2>
+        <div><b>temperature: </b>{`${weather.current.temperature} Celsius`}</div>
+        <img src={weather.current.weather_icons[0]} alt={`${props.country.capital} weather`} height="50" width="50"/>
+        <div><b>wind: </b>{`${weather.current.wind_speed} mph direction ${weather.current.wind_dir}`}</div>
+      </div>
+    )
+  } else if (weather.success === false){
+    return (
+      <div>Cannot find capital</div>
+    )
+  } else {
+    return (
+      <div>Loading weather...</div>
+    )
+  }
+}
+
+const Button = (props) => {
+  return (
+    <button value={props.countryName} onClick = {props.handleClick}>
+      {props.text}
+    </button>
+  )
 }
 
 export default App;
