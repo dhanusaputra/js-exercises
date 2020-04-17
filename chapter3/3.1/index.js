2cbba414c427985deabac032fe1af5dd124a3ba0
dhanu <andreasdhanu@gmail.com>
2020-04-17 07:38:34 +0700

    finish 3.8

--- chapter3/3.1/index.js
+++ chapter3/3.1/index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
-app.use(express.json())
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
+app
+  .use(express.json())
+  .use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req, res)
+  ].join(' ')
+}))
 
 let persons = [
     {
