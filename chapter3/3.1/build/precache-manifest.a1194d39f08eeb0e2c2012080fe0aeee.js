c41535a4bdc8ec9257674c0b1ff531385dd67ada
dhanu <andreasdhanu@gmail.com>
2020-04-19 19:50:22 +0700

    fix

---
+++ chapter3/3.1/build/precache-manifest.a1194d39f08eeb0e2c2012080fe0aeee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc17d36e41124831aeac18c98705434b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41edf0c47696062529f4",
+    "url": "/static/css/main.6dd13fc8.chunk.css"
+  },
+  {
+    "revision": "ff60a351c4742c3ecaa3",
+    "url": "/static/js/2.ec9a902a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec9a902a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41edf0c47696062529f4",
+    "url": "/static/js/main.1ac3303c.chunk.js"
+  },
+  {
+    "revision": "8af90391a258962e398b",
+    "url": "/static/js/runtime-main.a494eec6.js"
+  }
+]);
