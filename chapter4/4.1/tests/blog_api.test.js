f4775ec8656cbae9a869dd4d6b4be71a0a5f579c
dhanu <andreasdhanu@gmail.com>
2020-04-23 08:56:00 +0700

    add test api

---
+++ chapter4/4.1/tests/blog_api.test.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title:'aaa',
+    author:'bbb',
+    url:'ccc.com',
+    likes:10,
+  },
+  {
+    title:'ddd',
+    author:'eee',
+    utl:'fff.com',
+    likes:20,
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
