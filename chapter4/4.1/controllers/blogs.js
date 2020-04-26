f4fb427769a6f81896290d4c26d76e228ccca61d
dhanu <andreasdhanu@gmail.com>
2020-04-26 23:35:03 +0700

    add token

--- chapter4/4.1/controllers/blogs.js
+++ chapter4/4.1/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -10,7 +11,10 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
   const user = await User.findById(body.userId)
   
   const blog = new Blog({
@@ -33,6 +37,13 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (blog.user) {
+    if (!request.token || decodedToken.id !== blog.user.toString()) {
+      return response.status(403).json({ error: 'deletion not allowed' })
+    }
+  }
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
--- chapter4/4.1/tests/blog_api.test.js
+++ chapter4/4.1/tests/blog_api.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -14,19 +15,29 @@ const initialBlogs = [
   {
     title:'ddd',
     author:'eee',
-    utl:'fff.com',
+    url:'fff.com',
     likes:20,
   }
 ]
 
+const user = {
+  username: 'admin',
+  password: 'admin',
+  name: 'admin',
+}
+
 beforeEach(async () => {
   await Blog.deleteMany({})
+  await User.deleteMany({})
 
   let blogObject = new Blog(initialBlogs[0])
   await blogObject.save()
 
   blogObject = new Blog(initialBlogs[1])
   await blogObject.save()
+
+  let userObject = new User(user)
+  await userObject.save()
 })
 
 test('blogs are returned as json', async () => {
@@ -48,16 +59,19 @@ test('blogs are returned with id', async () => {
 })
 
 test('a valid blog can be added', async() => {
+  const user = await api.get('/api/users')
   const newBlog = {
     title: 'test',
     author: 'q',
     url: 'aaa.com',
     likes: 5,
+    userId: user.body[0].id,
   }
 
   await api
     .post('/api/blogs')
     .send(newBlog)
+    .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWQiOiI1ZWE1YjQ5MTcyNjgxYjBiZWVjMjE2MzEiLCJpYXQiOjE1ODc5MTc5OTB9.BR9qtqMJhrCetcotLG16C4-qG8cCGc5w1hUbIj2UiLU')
     .expect(201)
     .expect('Content-Type', /application\/json/)
 
@@ -70,15 +84,18 @@ test('a valid blog can be added', async() => {
 })
 
 test('likes default value is 0', async() => {
+  const user = await api.get('/api/users')
   const newBlog = {
     title: 'ccc',
     author: 'q',
     url: '123.com',
+    userId: user.body[0].id,
   }
 
   await api
     .post('/api/blogs')
     .send(newBlog)
+    .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWQiOiI1ZWE1YjQ5MTcyNjgxYjBiZWVjMjE2MzEiLCJpYXQiOjE1ODc5MTc5OTB9.BR9qtqMJhrCetcotLG16C4-qG8cCGc5w1hUbIj2UiLU')
     .expect(201)
     .expect('Content-Type',/application\/json/)
 
@@ -88,15 +105,35 @@ test('likes default value is 0', async() => {
 })
 
 test('400 when url and title are missing', async() => {
+  const user = await api.get('/api/users')
   const newBlog = {
     title: 'aa',
     author: 'q',
+    userId: user.body[0].id,
   }
 
   await api
     .post('/api/blogs')
     .send(newBlog)
+    .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWQiOiI1ZWE1YjQ5MTcyNjgxYjBiZWVjMjE2MzEiLCJpYXQiOjE1ODc5MTc5OTB9.BR9qtqMJhrCetcotLG16C4-qG8cCGc5w1hUbIj2UiLU')
     .expect(400)
+})
+
+test('401 when token not provided', async() => {
+  const user = await api.get('/api/users')
+  const newBlog = {
+    title: 'test',
+    author: 'q',
+    url: 'aaa.com',
+    likes: 5,
+    userId: user.body[0].id,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(401)
+    .expect('Content-Type', /application\/json/)
 })
 
 test('deletion of a blog', async() => {
@@ -105,6 +142,7 @@ test('deletion of a blog', async() => {
   
   await api
     .delete(`/api/blogs/${blogToDelete.id}`)
+    .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWQiOiI1ZWE1YjQ5MTcyNjgxYjBiZWVjMjE2MzEiLCJpYXQiOjE1ODc5MTc5OTB9.BR9qtqMJhrCetcotLG16C4-qG8cCGc5w1hUbIj2UiLU')
     .expect(204)
 
   const responseEnd = await api.get('/api/blogs')
@@ -121,7 +159,6 @@ test('update of a blog', async() => {
     title: '000'
   }
 
-  console.log(blogToUpdate)
   await api
     .put(`/api/blogs/${blogToUpdate.id}`)
     .send(newBlog)
