b29ef3cdc49c0fddc13c1e90b39a7dca2532073b
dhanu <andreasdhanu@gmail.com>
2020-05-22 15:39:12 +0700

    start

---
+++ chapter9/9.1/bmiCalculator.ts
@@ -0,0 +1,31 @@
+const calculateBMI = (h: number, w: number) : string => {
+  switch(true) {
+  case w/Math.pow(h*0.01, 2) < 15:
+    return 'Very severely underweight';
+  case w/Math.pow(h*0.01, 2) < 16:
+    return 'Severely underweight';
+  case w/Math.pow(h*0.01, 2) < 18.5:
+    return 'Underweight';
+  case w/Math.pow(h*0.01, 2) < 25:
+    return 'Normal (healthy weight)';
+  case w/Math.pow(h*0.01, 2) < 30:
+    return 'Overweight';
+  case w/Math.pow(h*0.01, 2) < 35:
+    return 'Obese Class I (Moderately obese)';
+  case w/Math.pow(h*0.01, 2) < 40:
+    return 'Obese Class II (Severely obese)';
+  case w/Math.pow(h*0.01, 2) >= 40:
+    return 'Obese Class III (Very severely obese)';
+  default:
+    throw new Error('invalid argument');
+  }
+}
+
+const h: number = Number(process.argv[2])
+const w: number = Number(process.argv[3])
+
+try {
+  console.log(calculateBMI(h, w))
+} catch (e) {
+  console.log('Something went wrong, error message: ', e.message);
+}
---
+++ chapter9/9.1/exerciseCalculator.ts
@@ -0,0 +1,42 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (args: Array<number>) : Result => {
+  const target: number = 2;
+  const average: number = args.reduce((acc, cur) => acc + cur, 0) / args.length;
+  let rating: number, ratingDescription: string;
+
+  switch (true) {
+  case Math.round(average) == target:
+    rating = 2;
+    ratingDescription = 'not bad';
+    break;
+  case Math.round(average) < target:
+    rating = 1;
+    ratingDescription = 'bad';
+    break;
+  case Math.round(average) > target:
+    rating = 3;
+    ratingDescription = 'good job';
+    break;
+  }
+
+  return {
+    periodLength: args.length,
+    trainingDays: args.reduce((acc, cur) => cur > 0 ? acc + 1 : acc, 0),
+    success: rating >= 2,
+    rating: rating,
+    ratingDescription: ratingDescription,
+    target: target,
+    average: average,
+  }
+}
+
+console.log(calculateExercises(process.argv.slice(2).map(Number)))
