d486b956bd70e0fd81d461eef1184325c3967d79
dhanu <andreasdhanu@gmail.com>
2020-05-26 09:46:25 +0700

    refactor action

--- chapter9/9.9/src/state/reducer.ts
+++ chapter9/9.9/src/state/reducer.ts
@@ -11,6 +11,20 @@ export type Action =
       payload: Patient;
     };
 
+export const setPatientList = (payload: Patient[]) => {
+  return {
+    type: "SET_PATIENT_LIST" as const,
+    payload,
+  };
+};
+
+export const addPatient = (payload: Patient) => {
+  return {
+    type: "ADD_PATIENT" as const,
+    payload,
+  };
+};
+
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case "SET_PATIENT_LIST":
