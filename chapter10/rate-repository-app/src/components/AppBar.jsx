66c47a1cae13eb451dee72444992bf19f2e21bcc
dhanu <andreasdhanu@gmail.com>
2021-01-05 22:59:20 +0700

    add

--- chapter10/rate-repository-app/src/components/AppBar.jsx
+++ chapter10/rate-repository-app/src/components/AppBar.jsx
@@ -6,23 +6,33 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight + 10,
     paddingBottom: 10,
-    paddingHorizontal: 10,
     backgroundColor: '#24292e',
+    flexDirection: 'row',
   },
   baseText: {
     color: 'white'
   },
+  button: {
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
 });
 
 const AppBar = () => {
-  return <View style={styles.container}>{
-    <TouchableWithoutFeedback>
-      <View>
-        <Text style={styles.baseText}>Repositories</Text>
-      </View>
-    </TouchableWithoutFeedback>
-  }</View>;
-  
+  return ( 
+    <View style={styles.container}>
+      <TouchableWithoutFeedback>
+        <View style={styles.button}>
+          <Text style={styles.baseText}>Sign in</Text>
+        </View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback>
+        <View style={styles.button}>
+          <Text style={styles.baseText}>Repositories</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  ); 
 };
 
 export default AppBar;
--- chapter10/rate-repository-app/src/components/Main.jsx
+++ chapter10/rate-repository-app/src/components/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Route, Switch } from 'react-router-native';
 
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,7 +18,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Switch>
+        <Route path="/" exact component={RepositoryList}/>
+        <Route path="/signin" exact component={SignIn}/>
+      </Switch>
     </View>
   );
   
