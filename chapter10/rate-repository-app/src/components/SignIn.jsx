7052e57e8bfb77bcc21cf825bb2202a6dfbc6686
dhanu <andreasdhanu@gmail.com>
2021-01-09 19:55:23 +0700

    add

--- chapter10/rate-repository-app/src/components/SignIn.jsx
+++ chapter10/rate-repository-app/src/components/SignIn.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { View, TouchableWithoutFeedback } from 'react-native';
+import { Formik } from 'formik';
 
+import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+
 const SignIn = () => {
-    return <Text>The sign in view</Text>;
-  
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      <View>
+        <FormikTextInput name='username' placeholder='Username' />
+        <FormikTextInput name='password' placeholder='Password' secureTextEntry />
+        <TouchableWithoutFeedback onPress={onSubmit}>
+          <Text>Sign in</Text>
+        </TouchableWithoutFeedback>
+      </View>
+    </Formik>
+  );
 };
 
 export default SignIn;
